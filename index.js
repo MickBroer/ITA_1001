bb050d1b81024d0a5fcf42343e882e1e9dbddf30
Mick Broer <mickbroerxoxo@gmail.com>
2023-10-17 09:06:23 +0200

    changed layout

--- index.js
+++ index.js
@@ -1,55 +1,68 @@
+let mediaRecorder;
+let audioChunks = [];
+let audioBlob;
+let audioUrl;
+let audio;
+let isRecording = false;
+let isPlaying = false;
 
-    let mediaRecorder;
-    let audioChunks = [];
-    let audioBlob;
-    let audioUrl;
-    let audio;
-    let isRecording = false;
+const toggleButton = document.getElementById("toggleButton");
+const playButton = document.getElementById("playButton");
 
-    const toggleButton = document.getElementById("toggleButton");
+toggleButton.addEventListener("click", async () => {
+  if (!isRecording) {
+    toggleButton.style.backgroundColor = "red";
+    toggleButton.textContent = "stoppen";
 
-    toggleButton.addEventListener("click", async () => {
-      if (!isRecording) {
-        // Change the button color and text when recording starts
-        toggleButton.style.backgroundColor = "red";
-        toggleButton.textContent = "Stop Recording";
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaRecorder = new MediaRecorder(stream);
+    audioChunks = [];
 
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        mediaRecorder = new MediaRecorder(stream);
-        audioChunks = [];
+    mediaRecorder.ondataavailable = event => {
+      audioChunks.push(event.data);
+    };
 
-        mediaRecorder.ondataavailable = event => {
-          audioChunks.push(event.data);
-        };
+    mediaRecorder.onstop = () => {
+      audioBlob = new Blob(audioChunks, { type: "audio/mp3" });
+      audioUrl = URL.createObjectURL(audioBlob);
+      audio = new Audio(audioUrl);
+    };
 
-        mediaRecorder.onstop = () => {
-          audioBlob = new Blob(audioChunks, { type: "audio/mp3" });
-          audioUrl = URL.createObjectURL(audioBlob);
-          audio = new Audio(audioUrl);
-        };
+    mediaRecorder.start();
+    isRecording = true;
+  } else {
+    mediaRecorder.stop();
+    toggleButton.style.backgroundColor = "";
+    toggleButton.textContent = "opnemen";
+    isRecording = false;
+  }
+});
 
-        mediaRecorder.start();
-        isRecording = true;
-      } else {
-        // Change the button color and text back when recording stops
-        mediaRecorder.stop();
-        toggleButton.style.backgroundColor = "";
-        toggleButton.textContent = "Start Recording";
-        isRecording = false;
-      }
-    });
+playButton.addEventListener("click", () => {
+  if (audio) {
+    if (!isPlaying) {
+      audio.play();
+      playButton.textContent = "stoppen";
+      isPlaying = true;
 
-    document.getElementById("playButton").addEventListener("click", () => {
-      if (audio) {
-        audio.play();
-      }
-    });
+      audio.addEventListener("ended", () => {
+        playButton.textContent = "terugluisteren";
+        isPlaying = false;
+      });
+    } else {
+      audio.pause();
+      audio.currentTime = 0;
+      playButton.textContent = "terugluisteren";
+      isPlaying = false;
+    }
+  }
+});
 
-    document.getElementById("downloadButton").addEventListener("click", () => {
-      if (audioBlob) {
-        const a = document.createElement("a");
-        a.href = audioUrl;
-        a.download = "myRecording.wav";
-        a.click();
-      }
-    });
+document.getElementById("downloadButton").addEventListener("click", () => {
+  if (audioBlob) {
+    const a = document.createElement("a");
+    a.href = audioUrl;
+    a.download = "myRecording.wav";
+    a.click();
+  }
+});
